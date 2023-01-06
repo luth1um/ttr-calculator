dd2383fdcd5a1d3536bfca8243cedf5f74478129
luth1um <52032867+luth1um@users.noreply.github.com>
2023-01-06 16:34:54 +0100

    button

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import { HowTo } from "./how-to/HowTo";
 
+function doNothing(): void {
+  console.log("button clicked");
+}
+
 function App() {
   return (
     <div>
       <h1>🏓 TTR Calculator</h1>
       <HowTo />
-      <Button variant="primary">Primary</Button>
+      <Button variant="primary" onClick={() => doNothing()}>
+        Primary
+      </Button>
     </div>
   );
 }

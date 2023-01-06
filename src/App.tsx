bbe71a8e941da8e424f521c623579206551ce0a0
luth1um <52032867+luth1um@users.noreply.github.com>
2023-01-06 15:47:31 +0100

    title and some basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
     <div>
+      <h1>🏓 TTR Calculator</h1>
+      <Card border="primary" style={{ maxWidth: '400px' }}>
+        <Card.Header as="h5">🤔 How To</Card.Header>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some text.
+          </Card.Text>
+        </Card.Body>
+      </Card>
       <Button variant="primary">Primary</Button>{' '}
     </div>
   );

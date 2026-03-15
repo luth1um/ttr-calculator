bc2e2a9ed425c4466c211c1ca2da1955fe9630d0
luth1um <52032867+luth1um@users.noreply.github.com>
2026-03-15 14:33:47 +0100

    basic dependency setup for modernization

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/ttr-calculator/',
+  base: "/ttr-calculator/",
   build: {
-    outDir: 'dist/ttr-calculator',
+    outDir: "dist/ttr-calculator",
   },
   server: {
     open: true,

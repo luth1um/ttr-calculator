89e7d1cc2a20e51c346667017a7dee0807080785
luth1um <52032867+luth1um@users.noreply.github.com>
2026-04-06 17:41:33 +0200

    slightly increased allowed pixel diff

--- e2e/visual.spec.ts
+++ e2e/visual.spec.ts
@@ -11,7 +11,7 @@ test.describe("The visuals of the page", () => {
     await appPage.goto();
 
     // then
-    await expect(page).toHaveScreenshot(createSnapshotName("initial-page", testInfo), screenshotOptions(0.02));
+    await expect(page).toHaveScreenshot(createSnapshotName("initial-page", testInfo), screenshotOptions(0.03));
   });
 
   test("should be as expected when when there is one opponent with a win and one opponent with a loss", async ({
@@ -27,7 +27,7 @@ test.describe("The visuals of the page", () => {
     await appPage.scrollToBottom();
 
     // then
-    await expect(page).toHaveScreenshot(createSnapshotName("one-win-one-loss", testInfo), screenshotOptions(0.02));
+    await expect(page).toHaveScreenshot(createSnapshotName("one-win-one-loss", testInfo), screenshotOptions(0.03));
   });
 
   test("should be as expected when a result is visible", async ({ page }, testInfo) => {
@@ -41,7 +41,7 @@ test.describe("The visuals of the page", () => {
     await appPage.scrollToBottom();
 
     // then
-    await expect(page).toHaveScreenshot(createSnapshotName("with-result", testInfo), screenshotOptions(0.02));
+    await expect(page).toHaveScreenshot(createSnapshotName("with-result", testInfo), screenshotOptions(0.03));
   });
 
   test("should be as expected when the reset dialog is visible", async ({ page }, testInfo) => {
@@ -55,7 +55,7 @@ test.describe("The visuals of the page", () => {
     await appPage.clickReset();
 
     // then
-    await expect(page).toHaveScreenshot(createSnapshotName("reset-dialog", testInfo), screenshotOptions(0.02));
+    await expect(page).toHaveScreenshot(createSnapshotName("reset-dialog", testInfo), screenshotOptions(0.03));
   });
 
   test("should be as expected when the results are stale", async ({ page }, testInfo) => {
@@ -70,7 +70,7 @@ test.describe("The visuals of the page", () => {
     await appPage.scrollToBottom();
 
     // then
-    await expect(page).toHaveScreenshot(createSnapshotName("stale-results", testInfo), screenshotOptions(0.02));
+    await expect(page).toHaveScreenshot(createSnapshotName("stale-results", testInfo), screenshotOptions(0.03));
   });
 });
 
